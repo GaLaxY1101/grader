1d741303785ec9c7d66bd2272d7aa99f027ce290
Illia Korniichyk <korniykhik3@gmail.com>
2026-04-05 10:51:43 +0300

    feat: init project with depencencies

---
+++ src/test/java/ua/kpi/grader/GraderApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kpi.grader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
